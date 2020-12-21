0cf7a607acc7c87f78e408221fe8222ea393301e
taoqf <tao_qiufeng@126.com>
2020-12-21 12:38:02 +0800

    parse file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,66 @@
-export default function fun() {
+import { IncomingMessage } from 'http';
+import { Form } from 'multiparty';
+import anylogger from 'anylogger';
+import ff from 'ffprobe';
+import config from '@mmstudio/config';
+
+const logger = anylogger('getfiles');
+
+const maxFilesSize = config.max_file_size as number;
+
+export interface IMetaData {
+	originialfilename?: string
+	'content-type'?: string
+	video?: ff.FFProbeStream
+	audio?: ff.FFProbeStream
+	screenshot?: string
+	duration?: number
+}
+
+export interface IFile {
+	id?: string
+	name: string
+	path: string
+	type: string
+	meta?: IMetaData
+}
+
+type ParsedFiles = Record<
+	string,
+	[
+		{
+			fieldName: string
+			path: string
+			originalFilename: string
+			headers: Record<string, string>
+			size: number
+		}
+	]
+>
+
+export default function parsefiles(req: IncomingMessage) {
+	return new Promise<IFile[]>((res, rej) => {
+		const form = new Form({
+			maxFilesSize,
+		});
+		form.parse(req, (err, _fields, files: ParsedFiles) => {
+			if (err) {
+				logger.error(err);
+				rej(err);
+			} else {
+				logger.debug('files parsed', files);
+				res(
+					Object.keys(files).map((name) => {
+						const [file] = files[name];
+						logger.debug('file parsed', file);
+						return {
+							name: file.fieldName,
+							path: file.path,
+							type: file.headers['content-type'] || 'application/octet-stream',
+						};
+					})
+				);
+			}
+		});
+	});
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,21 @@
 const test = require('ava');
+const http = require('http');
+const an40 = require('@mmstudio/an000040').default;
 
 const { default: a } = require('../dist/index');
+const port = 3000;
+const name = 'mm';
 
-test('xxx', (t) => {
-	t.pass();
+test('parse file', async (t) => {
+	let server;
+	const [file] = await (() => {
+		return new Promise((res) => {
+			server = http.createServer((req) => {
+				res(a(req));
+			}).listen(port);
+			an40(`curl --form "${name}=@./tests/test.js" http://127.0.0.1:${port}`)
+		});
+	})();
+	server.close();
+	t.is(file.name, name);
 });
